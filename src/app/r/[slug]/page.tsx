a6095e4cd10e45f93cbab80908073e75d5db7b3b
devwilson <ohioleomole@gmail.com>
2025-12-11 13:52:16 +0000

    Add Create post page

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import MiniCreatePost from "@/components/MiniCreatePost";
+import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { notFound } from "next/navigation";
 
@@ -11,6 +13,8 @@ interface PageProps {
 
 const page = async ({ params }: PageProps) => {
   const { slug } = params;
+
+  const session = await getAuthSession();
 
   const subreddit = await db.subreddit.findFirst({
     where: { name: slug },
@@ -37,6 +41,7 @@ const page = async ({ params }: PageProps) => {
       <h1 className="font-bold text-3xl md:text-4xl h-14">
         r/{subreddit.name}
       </h1>
+      <MiniCreatePost session={session} />
     </>
   );
 };
